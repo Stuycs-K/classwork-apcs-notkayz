e2f27ee1aef5ef04a02751407636ffa20cdb7e95
Kayden Au <aukayden@gmail.com>
2024-10-01 14:47:46 -0400

    wrote replaceNegative working on test case

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -19,6 +19,8 @@ public class ArrayMethods{
     // test cases for swapRC
     System.out.println("Expected: [[1, 4], [2, 5], [3, 6]] vs my function: " + arrToString(swapRC(new int[][]{{1, 2 , 3}, {4, 5 , 6}})));
     System.out.println("Expected: [[9, 8], [3, 2], [7, 4]] vs my function: " + arrToString(swapRC(new int[][]{{9, 3, 7}, {8, 2, 4}})));
+
+    //System.out.println("Expected: [[1, 0, 0], [0, 1, 0]] vs my function: " + replaceNegative(new int[][] {{-2, -3, -2}, {-9, -239, -10}}));
   }
 
   public static String arrToString(int[] nums) {
@@ -44,7 +46,7 @@ public class ArrayMethods{
   }
 
   public static int arr2DSum(int[][] nums) {
-    int sum = 0; 
+    int sum = 0;
     for (int i = 0; i < nums.length; i++) {
       for (int j = 0; j < nums[i].length; j++) {
         sum += nums[i][j];
@@ -61,5 +63,32 @@ public class ArrayMethods{
       }
     }
     return swappedArr;
+  }
+  //3. Modify a given 2D array of integer as follows:
+  //Replace all the negative values:
+  //-When the row number is the same as the column number replace
+  //that negative with the value 1
+  //-All other negatives replace with 0
+  public static void replaceNegative(int[][] vals){
+    for (int i = 0; i < vals.length; i++) {
+      for (int j = 0; j < vals[i].length; j++) {
+        if (j == i && vals[i][j] < 0) {
+          vals[i][j] = 1;
+        }
+        if (vals[i][j] < 0) {
+          vals[i][j] = 0;
+        }
+      }
+    }
+    System.out.println(arrToString(vals));
+  }
+
+  //4. Make a copy of the given 2d array.
+  //When testing : make sure that changing the original does NOT change the copy.
+  //DO NOT use any built in methods that "copy" an array.
+  //You SHOULD write a helper method for this.
+  //If you don't see a good way to do that, you should stop and look at prior methods.
+  public static int[][] copy(int[][] nums){
+    return null;//placeholder so it compiles
   }
 }
