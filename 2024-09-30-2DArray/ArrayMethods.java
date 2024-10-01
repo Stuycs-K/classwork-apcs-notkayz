efaa1be3a665b8b06b883220085ea05394e8d8ac
Kayden Au <aukayden@gmail.com>
2024-10-01 01:47:10 -0400

    completed array methods and added 2 test cases for each

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -3,13 +3,25 @@
 
 public class ArrayMethods{
   public static void main(String args[]){
-    //System.out.println("Expected: [2, 3, 4, 9] vs my function: " + aryToString(new int[2, 3, 4, 9]));
-    System.out.println("Expected: {{2, 3, 4}, {5, 6, 7}, {2, 4, 9}} vs my function: " + arrToString(new int[][]{{2, 3, 4}, {5, 6, 7}, {2, 4, 9}}));
-    //System.out.println("Expected: 42 vs my function: " + arr2DSum(new int[][]{{2, 3, 4}, {5, 6, 7}, {2, 4, 9}}));
-    //System.out.println("Expected: {{1, 4}, {2, 5}, {3, 6}} vs my function: " + swapRC(new int[] [] {{1, 2, 3}, {4, 5, 6}}));
+
+    // test cases for 1D arrToString
+    System.out.println("Expected: [2, 3, 4, 9] vs my function: " + arrToString(new int[] {2, 3, 4, 9}));
+    System.out.println("Expected: [10, 42, 1, 124] vs my function: " + arrToString(new int[] {10, 42, 1, 124}));
+
+    // test cases for 2D arrToString
+    System.out.println("Expected: [[2, 3, 4], [5, 6, 7], [2, 4, 9]] vs my function: " + arrToString(new int[][]{{2, 3, 4}, {5, 6, 7}, {2, 4, 9}}));
+    System.out.println("Expected: [[10, 42, 1, 124], [40, 30, 72], [8, 44, 0]] vs my function: " + arrToString(new int[][] {{10, 42, 1, 124}, {40, 30, 72}, {8, 44, 0}}));
+
+    // test cases for arr2DSum
+    System.out.println("Expected: 42 vs my function: " + arr2DSum(new int[][]{{2, 3, 4}, {5, 6, 7}, {2, 4, 9}}));
+    System.out.println("Expected: 499 vs my function: " + arr2DSum(new int[][]{{10, 42, 124}, {25, 68, 73}, {20, 42, 95}}));
+
+    // test cases for swapRC
+    System.out.println("Expected: [[1, 4], [2, 5], [3, 6]] vs my function: " + arrToString(swapRC(new int[][]{{1, 2 , 3}, {4, 5 , 6}})));
+    System.out.println("Expected: [[9, 8], [3, 2], [7, 4]] vs my function: " + arrToString(swapRC(new int[][]{{9, 3, 7}, {8, 2, 4}})));
   }
 
-  public static String aryToString(int[] nums) {
+  public static String arrToString(int[] nums) {
     String ans = "[";
     for (int i = 0; i < nums.length; i++) {
       ans += nums[i];
@@ -21,23 +33,33 @@ public class ArrayMethods{
   }
 
   public static String arrToString(int[][] nums) {
-    String ans = "{{";
+    String ans = "[";
     for (int i = 0; i < nums.length; i++){
-      ans += aryToString(nums[i]);
-    }
-    for (int i = 0; i < nums.length; i++) {
-      //ans += nums[i];
+      ans += arrToString(nums[i]);
       if (i < nums.length - 1) {
         ans += ", ";
-      for (int j = 0; j < nums[i].length; j++) {
-        ans += nums[i][j];
-        if (j < nums[i].length - 1) {
-          ans += ", ";
-        }
       }
     }
-    ans += "}";
+    return ans + "]";
   }
-  return ans + "}}";
-}
+
+  public static int arr2DSum(int[][] nums) {
+    int sum = 0; 
+    for (int i = 0; i < nums.length; i++) {
+      for (int j = 0; j < nums[i].length; j++) {
+        sum += nums[i][j];
+      }
+    }
+    return sum;
+  }
+
+  public static int[][] swapRC(int[][] nums) {
+    int[][] swappedArr = new int[nums[0].length][nums.length];
+    for (int i = 0; i < nums.length; i++) {
+      for (int j = 0; j < nums[i].length; j++) {
+        swappedArr[j][i] = nums[i][j];
+      }
+    }
+    return swappedArr;
+  }
 }
