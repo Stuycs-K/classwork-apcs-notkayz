527da20e1a7700f06cebbb71c9206cea01db00c0
Kayden Au <aukayden@gmail.com>
2024-11-01 23:46:45 -0400

    fixed countTrianglesA

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -9,14 +9,25 @@ public class TriangleTester {
 
   public static int countTrianglesA(String filename) {
     int res = 0;
-    while (filename.hasNextLine())
-    {
-      if (isTriangle(filename.nextDouble(), filename.nextDouble(), filename.nextDouble()))res++;
+    try {
+      File file = new File(filename);
+      Scanner input = new Scanner(file);
+      while (input.hasNextLine()) {
+        if (isTriangle(input.nextDouble(), input.nextDouble(), input.nextDouble()))
+          res++;
+      }
+      input.close();
+      return res;
+    } catch (FileNotFoundException e) {
+      System.out.println("File not found");
+      return -1;
     }
-    return res;
   }
-  public static boolean isTriangle(double A, double B, double C){
-    if (A+B > C && B+C > A && C+A > B)return true;
+
+  public static boolean isTriangle(double a, double b, double c) {
+    if (a + b > c && b + c > a && c + a > b) {
+      return true;
+    }
     return false;
   }
 }
