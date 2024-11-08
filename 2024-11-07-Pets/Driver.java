8766ba1a4dc97c0b9f824a74317a72c825e1d418
Kayden Au <aukayden@gmail.com>
2024-11-07 20:43:50 -0500

    step 9: tested which types can be assigned to which variables

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -7,5 +7,12 @@ public class Driver{
     toucan.speak();
     Bird LennyJr = new Bird("SQUACK", 2, "Lenny Jr", 10.5, "rainbow");
     LennyJr.speak();
+
+    // step 9
+    Animal a1 = new Animal("moo", 1, "cow");
+    Bird b1 = new Bird("caw caw", 2, "raven", 25, "black");
+    // Bird b2 = new Animal("meow", 3, "birb"); this one doesnt work 
+    Animal a2 = new Bird("chirp chirp", 2, "max", 20, "blue");
+
   }
 }
