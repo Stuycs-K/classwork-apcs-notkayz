c35fe41e69c9bf418cbd8321ed713bf2ceccd3b6
Kayden Au <aukayden@gmail.com>
2024-11-18 23:40:44 -0500

    debugging + main idea; substring(1) gives the whole thing substring(2) gives the number

--- 2024-11-18-advent2016/day1.java
+++ 2024-11-18-advent2016/day1.java
@@ -6,7 +6,9 @@ public class day1 {
 
   public static int part1 (String filename) {
     try {
-      int distance = 0;
+      int x = 0;
+      int y = 0;
+      int direction = 0;    // 0 = north, 1 = east, 2 = south, 3 = west
       File f1 = new File (filename);
       Scanner input = new Scanner(f1);
       while (input.hasNextLine()) {
@@ -15,9 +17,43 @@ public class day1 {
         // for (int i = 0; i < moves.length; i++) {
         //   System.out.println(moves[i]);
         // }
+        for (int i = 0; i < moves.length; i++) {
+          System.out.println(moves[i].substring(2));
+          // if (direction % 4 == 0) {
+          //   if (moves[i].contains("R")) {
+          //     direction++; 
+          //     x += Integer.parseInt(moves[i].substring(1));
+          //   }
+          //   if (moves[i].contains("L")) {
+          //     direction += 3; 
+          //     x -= Integer.parseInt(moves[i].substring(1));
+          //   }
+          // }
+
+          // if (direction % 4 == 2) {
+          //   if (moves[i].contains("R")) {
+          //     direction++; 
+          //     x -= Integer.parseInt(moves[i].substring(1));
+          //   }
+          //   if (moves[i].contains("L")) {
+          //     direction += 3; 
+          //     x += Integer.parseInt(moves[i].substring(1));
+          //   }
+          // }
+          //   if (direction % 4 == 3) {
+          //     if (moves[i].contains("R")) {
+          //       direction++; 
+          //       y += Integer.parseInt(moves[i].substring(1));
+          //     }
+          //     if (moves[i].contains("L")) {
+          //       direction += 3; 
+          //       y -= Integer.parseInt(moves[i].substring(1));
+          //     }
+          // }
+        }
       }
       input.close();
-      return distance;
+      return Math.abs(x) + Math.abs(y);
     }
     catch (FileNotFoundException ex) {
       System.out.println("File not found!");
