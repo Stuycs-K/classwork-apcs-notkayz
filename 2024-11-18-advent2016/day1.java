adb20429d7df0d22b3de61c6419757a1c7291b07
Kayden Au <aukayden@gmail.com>
2024-11-19 00:16:03 -0500

    different version; number still too high

--- 2024-11-18-advent2016/day1.java
+++ 2024-11-18-advent2016/day1.java
@@ -4,92 +4,115 @@ import java.util.Scanner;
 
 public class day1 {
 
-  public static int part1 (String filename) {
+  public static int part1(String filename) {
     try {
       int x = 0;
       int y = 0;
-      int direction = 0;    // 0 = north, 1 = east, 2 = south, 3 = west
-      File f1 = new File (filename);
+      int dir = 0;
+      File f1 = new File(filename);
       Scanner input = new Scanner(f1);
+
       while (input.hasNextLine()) {
         String line = input.nextLine();
         String[] moves = line.split(",");
         // for (int i = 0; i < moves.length; i++) {
-        //   System.out.println(moves[i]);
+        // System.out.println(moves[i]);
         // }
+
         for (int i = 0; i < moves.length; i++) {
           // System.out.println(moves[i].substring(2));
-          if (direction % 4 == 0) {
-            if (moves[i].contains("R")) {
-              direction++;
-              if (!moves[i].substring(2).isEmpty()) {
-                x += Integer.parseInt(moves[i].substring(2));
-              }
+          moves[i].trim();
+          if (!moves[i].substring(2).isEmpty()) {
+            int forwardSteps = Integer.parseInt(moves[i].substring(2));
+            String lOrR = moves[0];
+            if (lOrR == "R") {
+              dir++;
+            } else if (lOrR == "L") {
+              dir += 3;
             }
-            if (moves[i].contains("L")) {
-              direction += 3; 
-              if (!moves[i].substring(2).isEmpty()) {
-                x -= Integer.parseInt(moves[i].substring(2));
-              }
+            if (dir % 4 == 0) {
+              y += forwardSteps;
             }
-          }
-
-          if (direction % 4 == 1) {
-            if (moves[i].contains("R")) {
-              direction++;
-              if (!moves[i].substring(2).isEmpty()) {
-                y -= Integer.parseInt(moves[i].substring(2));
-              }
+            if (dir % 4 == 1) {
+              x += forwardSteps;
             }
-            if (moves[i].contains("L")) {
-              direction += 3; 
-              if (!moves[i].substring(2).isEmpty()) {
-                y += Integer.parseInt(moves[i].substring(2));
-              }
+            if (dir % 4 == 2) {
+              y -= forwardSteps;
             }
-          }
-
-          if (direction % 4 == 2) {
-            if (moves[i].contains("R")) {
-              direction++; 
-              if (!moves[i].substring(2).isEmpty()) {
-                x -= Integer.parseInt(moves[i].substring(2));
-              }
+            if (dir % 4 == 3) {
+              x -= forwardSteps;
             }
-            if (moves[i].contains("L")) {
-              direction += 3; 
-              if (!moves[i].substring(2).isEmpty()) {
-                x += Integer.parseInt(moves[i].substring(2));
-              }
-            }
-          }
-
-            if (direction % 4 == 3) {
-              if (moves[i].contains("R")) {
-                direction++; 
-                if (!moves[i].substring(2).isEmpty()) {
-                  y += Integer.parseInt(moves[i].substring(2));
-                }
-              }
-              if (moves[i].contains("L")) {
-                direction += 3; 
-                if (!moves[i].substring(2).isEmpty()) {
-                  y -= Integer.parseInt(moves[i].substring(2));
-                }
-              }
           }
         }
       }
+      // if (direction % 4 == 0) {
+      // if (moves[i].contains("R")) {
+      // direction++;
+      // if (!moves[i].substring(2).isEmpty()) {
+      // x += Integer.parseInt(moves[i].substring(2));
+      // }
+      // }
+      // if (moves[i].contains("L")) {
+      // direction += 3;
+      // if (!moves[i].substring(2).isEmpty()) {
+      // x -= Integer.parseInt(moves[i].substring(2));
+      // }
+      // }
+      // }
+
+      // if (direction % 4 == 1) {
+      // if (moves[i].contains("R")) {
+      // direction++;
+      // if (!moves[i].substring(2).isEmpty()) {
+      // y -= Integer.parseInt(moves[i].substring(2));
+      // }
+      // }
+      // if (moves[i].contains("L")) {
+      // direction += 3;
+      // if (!moves[i].substring(2).isEmpty()) {
+      // y += Integer.parseInt(moves[i].substring(2));
+      // }
+      // }
+      // }
+
+      // if (direction % 4 == 2) {
+      // if (moves[i].contains("R")) {
+      // direction++;
+      // if (!moves[i].substring(2).isEmpty()) {
+      // x -= Integer.parseInt(moves[i].substring(2));
+      // }
+      // }
+      // if (moves[i].contains("L")) {
+      // direction += 3;
+      // if (!moves[i].substring(2).isEmpty()) {
+      // x += Integer.parseInt(moves[i].substring(2));
+      // }
+      // }
+      // }
+
+      // if (direction % 4 == 3) {
+      // if (moves[i].contains("R")) {
+      // direction++;
+      // if (!moves[i].substring(2).isEmpty()) {
+      // y += Integer.parseInt(moves[i].substring(2));
+      // }
+      // }
+      // if (moves[i].contains("L")) {
+      // direction += 3;
+      // if (!moves[i].substring(2).isEmpty()) {
+      // y -= Integer.parseInt(moves[i].substring(2));
+      // }
+      // }
+      // }
       input.close();
       return Math.abs(x) + Math.abs(y);
-    }
-    catch (FileNotFoundException ex) {
+    } catch (FileNotFoundException ex) {
       System.out.println("File not found!");
       return -1;
     }
   }
 
-  public static void main (String[] args) {
+  public static void main(String[] args) {
     System.out.println(part1("day1input.txt"));
   }
 }
