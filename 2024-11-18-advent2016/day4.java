1b7bbb68f94bf8dc9eab6b04db9bf257800e1367
Kayden Au <aukayden@gmail.com>
2024-11-19 22:58:52 -0500

    started workingon day4 logic is a bit wonky

--- 2024-11-18-advent2016/day4.java
+++ 2024-11-18-advent2016/day4.java
@@ -1,6 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.*;
 
 public class day4 {
     public static int solver(String filename) {
@@ -8,8 +8,28 @@ public class day4 {
             File f1 = new File(filename);
             Scanner input = new Scanner(f1);
             int sum = 0; 
+            while (input.hasNextLine()) {
+                String line = input.nextLine();
+                int[] counter = new int[26];
+                for (int i = 0; i < line.length(); i++) {
+                    if (Character.isLetter(line.charAt(i))) {
+                        counter[line.charAt(i) - 'a']++;
+                        // System.out.println(line.charAt(i));
+                    }
+                } 
+                // System.out.println(Arrays.toString(counter));
+            }
 
-            while ()
+            input.close();
+            return sum;
         }
+        catch (FileNotFoundException ex) {
+            System.out.println("File not found");
+            return -1;
+        }
+    }
+
+    public static void main (String[] args) { 
+        System.out.println(solver("day4input.txt"));
     }
 }
