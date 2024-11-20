338eac69cebe5821a73828144fe0a286c57a8abc
Kayden Au <aukayden@gmail.com>
2024-11-19 22:40:05 -0500

    renamed

---
+++ 2024-11-18-advent2016/day4.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class day4 {
+    public static int solver(String filename) {
+        try {
+            File f1 = new File(filename);
+            Scanner input = new Scanner(f1);
+            int sum = 0; 
+
+            while ()
+        }
+    }
+}
