6e1fc41ca19cc80bf02144294b6bf58d00e3dafa
Kayden Au <aukayden@gmail.com>
2024-11-20 23:16:56 -0500

    need to figure out a way to initalize the 2darray

--- 2024-11-18-advent2016/day6.java
+++ 2024-11-18-advent2016/day6.java
@@ -3,14 +3,21 @@ import java.io.FileNotFoundException;
 import java.util.*;
 public class day6 {
   public static void main (String[] args ) {
-
+    System.out.println(solver("day6input.txt"));
   }
   public static String solver(String filename) {
     try {
       File f1 = new File(filename);
       Scanner input = new Scanner(f1);
-      while (input.hasNextLine()) {
-      }
+      String message = "";
+
+      
+      input.close();
+      return message;
+    }
+    catch (FileNotFoundException ex) {
+      System.out.println("File not found");
+      return "";
     }
   }
 }
