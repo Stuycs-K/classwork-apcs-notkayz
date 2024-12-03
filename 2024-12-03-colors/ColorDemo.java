110ba21ac89002d9facea99fc23b0c5fc7082284
Kayden Au <aukayden@gmail.com>
2024-12-03 11:56:46 -0500

    classwork 12/03

---
+++ 2024-12-03-colors/ColorDemo.java
@@ -0,0 +1,26 @@
+public class ColorDemo {
+
+  // USEFUL SEQUENCES
+  public static final String CLEAR_SCREEN =  "\u001b[2J";
+  public static final String HIDE_CURSOR =  "\u001b[?25l";
+  public static final String SHOW_CURSOR =  "\u001b[?25h";
+
+  // COLORS
+  public static int DEFAULT = 0;
+  public static int BLACK = 30;
+  public static int RED = 31;
+  public static int GREEN = 32;
+  public static int YELLOW = 33;
+  public static int BLUE = 34;
+  public static int MAGENTA = 35;
+  public static int CYAN = 36;
+  public static int WHITE = 37;
+
+  public static void makeScreenRainbow() {
+    
+  }
+
+  public static void main (String[] args) {
+    makeScreenRainbow();
+  }
+}
