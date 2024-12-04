078456e97328023e546f267b8e15677d28366302
Kayden Au <aukayden@gmail.com>
2024-12-04 09:17:57 -0500

    tested with unix as input string

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -40,7 +40,7 @@ public class ColorDemo {
       for(int r = 0; r < 256; r+=16){
         for(int g = 0; g <= 256; g+=16){
           for(int b = 0; b <= 256; b+=16){
-            System.out.print("\u001b[38;2;"+r+";"+g+";"+b+";7m.");
+            System.out.print("\u001b[38;2;"+r+";"+g+";"+b+";7m");
             System.out.print(text + "  ");
           }
         }
@@ -54,6 +54,6 @@ public class ColorDemo {
   public static void main (String[] args) {
     // System.out.print(CLEAR_SCREEN);
     // makeScreenRainbow();
-    makeMyTextColorful("hahahahahaha", "rgb");
+    makeMyTextColorful("yea you suck", "unix");
   }
 }
