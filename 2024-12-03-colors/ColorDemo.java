ca871605ede973185ab8c80525c29ad643306264
Kayden Au <aukayden@gmail.com>
2024-12-04 09:09:09 -0500

    created a function that prints the input text 100 times in a "random" color

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -6,15 +6,15 @@ public class ColorDemo {
   public static final String SHOW_CURSOR =  "\u001b[?25h";
 
   // COLORS
-  public static int DEFAULT = 0;
-  public static int BLACK = 30;
-  public static int RED = 31;
-  public static int GREEN = 32;
-  public static int YELLOW = 33;
-  public static int BLUE = 34;
-  public static int MAGENTA = 35;
-  public static int CYAN = 36;
-  public static int WHITE = 37;
+  // public static int DEFAULT = 0;
+  // public static int BLACK = 30;
+  // public static int RED = 31;
+  // public static int GREEN = 32;
+  // public static int YELLOW = 33;
+  // public static int BLUE = 34;
+  // public static int MAGENTA = 35;
+  // public static int CYAN = 36;
+  // public static int WHITE = 37;
 
   public static int[] colors = new int[] {30, 31, 32, 33, 34, 35, 36, 37};
 
@@ -29,7 +29,16 @@ public class ColorDemo {
     }
   }
 
+  public static void makeMyTextColorful(String text) {
+    for (int i = 0; i < 100; i++) {
+      System.out.print("\u001b[" + ((int) (Math.random() * 7 + 30)) + "m");
+      System.out.print(text + "  ");
+    }
+  }
+
   public static void main (String[] args) {
-    makeScreenRainbow();
+    // System.out.print(CLEAR_SCREEN);
+    // makeScreenRainbow();
+    makeMyTextColorful("hahahahahaha");
   }
 }
