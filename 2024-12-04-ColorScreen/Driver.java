b7fac76c16238c08a1d6cf8363b25a2c1dfa2fe9
Kayden Au <aukayden@gmail.com>
2024-12-04 13:10:01 -0500

    vertical borders

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -2,14 +2,19 @@ public class Driver {
 
     public static void makeBorder(int colorID) {
         Text.color(Text.background(colorID));
-        Text.go(1, 1);
-        for (int i = 0; i < 30; i++) {
-            System.out.print("=");
+        for (int i = 1; i <= 80; i++) {
+            Text.go(i,1);
+            System.out.print("|");
+            Text.go(i,31);
+            System.out.print("|");
         }
-        Text.go(81, 1);
-        for (int i = 0; i < 30; i++) {
-            System.out.print("-");
-        }
+        // for (int i = 0; i < 30; i++) {
+        //     System.out.print("=");
+        // }
+        // Text.go(81, 1);
+        // for (int i = 0; i < 30; i++) {
+        //     System.out.print("-");
+        // }
     }
     public static void main (String[] args) {
         System.out.print(Text.CLEAR_SCREEN);
