8069c02942f032cd2865efa6b1b3629d14bf2752
Kayden Au <aukayden@gmail.com>
2024-12-06 13:58:49 -0500

    tests for creating instances and testing getSpecialName()

--- 2024-12-06-Adventurer/Driver.java
+++ 2024-12-06-Adventurer/Driver.java
@@ -1,5 +1,15 @@
 public class Driver {
   public static void main (String[] args) {
+
+    // new instances of mages
     Adventurer player1 = new Mage();
+    Adventurer player2 = new Mage();
+    Adventurer ally1 = new Mage();
+    Adventurer ally2 = new Mage();
+
+    // special name for mage
+    System.out.println("Special name is: " + player1.getSpecialName() + ". Expected: Sorcerer");
+
+    
   }
 }
