d90265ef6f9e0424faec7d8d04dfc7f9cb2782b2
Kayden Au <aukayden@gmail.com>
2024-12-06 13:49:04 -0500

    added abstract keyword before public class; fixed support methods

--- 2024-12-06-Adventurer/Mage.java
+++ 2024-12-06-Adventurer/Mage.java
@@ -1,11 +1,20 @@
-public class Mage extends {
+abstract public class Mage extends Adventurer{
+  
+  // instance variables
   private int mana, maxMana;
 
+  // no arg constructor
+  public Mage() {
+    super("Mage", 100);
+    int mana = 100;
+    int maxMana = 500;
+  }
+
+  //accessor methods
   public String getSpecialName() {
     return "Sorcerer";
   }
 
-  //accessor methods
   public int getSpecial() {
     return this.mana;
   }
@@ -27,11 +36,20 @@ public class Mage extends {
   public String support(Adventurer other) {
     if (other.getHP() + (mana / 2) > other.getmaxHP()) {
       other.setmaxHP(other.getmaxHP() + (mana / 2));
-      return "Buffed "
+      other.setHP(other.getHP() + (mana / 2));
+      return "Increased max health of " + other.getName() + "!";
     }
-    other.setHP(other.getHp() + (mana / 2));
+    other.setHP(other.getHP() + (mana / 2));
     return "Healed " + other.getName() + " for " + (mana / 2) + " health.";
   }
 
-  public String support()
+  public String support() {
+    if (this.getHP() + (mana / 2) > this.getmaxHP()) {
+      this.setmaxHP(this.getmaxHP() + (mana / 2));
+      this.setHP(this.getHP() + (mana / 2));
+      return "Increase my max hp by " + (mana / 2) + "!";
+    }
+    this.setHP(this.getHP() + (mana / 2));
+    return "Healed myself for " + (mana / 2) + " health.";
+  }
 }
