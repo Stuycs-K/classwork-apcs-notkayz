01d1dc7fc9dfe247d2aaf5cf472b3de5bf2200bb
Kayden Au <aukayden@gmail.com>
2024-12-10 22:30:11 -0500

    changed the question

--- DemoCounter2.java
+++ DemoCounter2.java
@@ -1,9 +1,9 @@
 import java.util.*;
 public class DemoCounter2 {
     public static void arrayMixer(int[] data) {
-        for (int i = 0; i < data.length - 2; i++) {
+        for (int i = 0; i < data.length - 1; i++) {
             if (data[i] < 10) {
-                data[i] = data[i + 2] - 4;
+                data[i] = data[i + 1] - 4;
             }
             else {
                 data[i] = data[i - 1] + data[i];
@@ -13,7 +13,6 @@ public class DemoCounter2 {
     public static void main(String[] args) {
         int[] nums = new int[] {2, 7, 18, 4, 5};
 
-        // 14, 0, 18, 4, 5
         arrayMixer(nums);
         System.out.println(Arrays.toString(nums));
     }
