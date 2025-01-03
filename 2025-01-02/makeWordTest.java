9c539491328dcf55aa9409165c6c7521048ac528
Kayden Au <aukayden@gmail.com>
2025-01-03 11:39:00 -0500

    base case is no letters left to add, prints out result

---
+++ 2025-01-02/makeWordTest.java
@@ -0,0 +1,16 @@
+public class makeWordTest {
+
+    public static void makeWords(int remainingLetters, String result, String alphabet) {
+        if (remainingLetters < 1) {
+            System.out.println(result);
+        }
+        else {
+            for (int i = 0; i < alphabet.length(); i++) {
+                
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
